c138a8424bc58004e3387a46a004a1d21b6cff4b
korczynsk1 <korczynskimm@gmail.com>
2019-10-26 01:44:01 +0200

    feature(editable-width-height): implementing new query parameters for width and height

--- chromium.js
+++ chromium.js
@@ -1,6 +1,6 @@
 const chrome = require('chrome-aws-lambda')
 
-async function getScreenshot(url, type, quality, fullPage) {
+async function getScreenshot(url, type, quality, fullPage, height, width) {
   // emoji support
   await chrome.font(
     'https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf'
@@ -14,7 +14,7 @@ async function getScreenshot(url, type, quality, fullPage) {
   })
 
   const page = await browser.newPage()
-  await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1 })
+  await page.setViewport({ width, height, deviceScaleFactor: 1 })
   await page.goto(url)
   const file = await page.screenshot({ type, quality, fullPage })
   await browser.close()
--- screenshot.js
+++ screenshot.js
@@ -5,9 +5,11 @@ const { getInt, getUrlFromPath, isValidUrl } = require('./validator')
 module.exports = async function(req, res) {
   try {
     const { pathname = '/', query = {} } = parse(req.url, true)
-    const { type = 'png', quality, fullPage } = query
+    const { type = 'png', quality, fullPage, height = 1920, width = 1080 } = query
     const url = getUrlFromPath(pathname)
     const qual = getInt(quality)
+    const h = getInt(height)
+    const w = getInt(width)
     if (!isValidUrl(url)) {
       res.statusCode = 400
       res.setHeader('Content-Type', 'text/html')
@@ -15,7 +17,7 @@ module.exports = async function(req, res) {
         `<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`
       )
     } else {
-      const file = await getScreenshot(url, type, qual, fullPage)
+      const file = await getScreenshot(url, type, qual, fullPage, h, w)
       res.statusCode = 200
       res.setHeader('Content-Type', `image/${type}`)
       res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
