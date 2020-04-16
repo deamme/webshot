db0533253fca3e93d316954011fb5d30719d55d8
deam <deamme@users.noreply.github.com>
2020-04-16 12:09:49 +0200

    Frontend update

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -7,11 +7,7 @@ import GithubCorner from 'react-github-corner'
 import './styles.scss'
 
 function PreviewCard({ src }) {
-  return (
-    <div className="preview-card">
-      {src && <img src={src} alt="Preview card" />}
-    </div>
-  )
+  return <div className="preview-card">{src && <img src={src} alt="Preview card" />}</div>
 }
 
 function Gallery({ children }) {
@@ -32,27 +28,15 @@ function Metatags() {
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://webshot.deam.io/" />
       <meta property="og:title" content="Webshot" />
-      <meta
-        property="og:description"
-        content="Screenshot websites as a Service"
-      />
-      <meta
-        property="og:image"
-        content="https://webshot.deam.io/https://webshot.deam.io/"
-      />
+      <meta property="og:description" content="Screenshot websites as a Service" />
+      <meta property="og:image" content="https://webshot.deam.io/https://webshot.deam.io/" />
 
       {/** Twitter  **/}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://webshot.deam.io" />
       <meta property="twitter:title" content="Webshot" />
-      <meta
-        property="twitter:description"
-        content="Screenshot websites as a Service"
-      />
-      <meta
-        property="twitter:image"
-        content="https://webshot.deam.io/https://webshot.deam.io/"
-      />
+      <meta property="twitter:description" content="Screenshot websites as a Service" />
+      <meta property="twitter:image" content="https://webshot.deam.io/https://webshot.deam.io/" />
     </Helmet>
   )
 }
@@ -75,46 +59,62 @@ function SearchBar(props) {
         <circle cx="11" cy="11" r="8"></circle>
         <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
       </svg>
-      <input
-        type="search"
-        placeholder="Enter a website e.g. https://stripe.com/"
-        {...props}
-      />
+      <input type="search" placeholder="Enter a website e.g. https://stripe.com/" {...props} />
     </div>
   )
 }
 
 export function App() {
   const [search, setSearch] = useState('')
-  const [website, setWebsite] = useState('')
+  const [website, setWebsite] = useState('expo.io')
+  const [option, setOption] = useState(0)
 
   let link
 
   if (website) {
-    link = 'https://webshot.deam.io/' + website
+    if (option === 0) {
+      link = 'https://webshot.deam.io/' + website
+    } else if (option === 1) {
+      link = 'https://webshot.deam.io/' + website + '?fullPage=true'
+    } else if (option === 2) {
+      link = 'https://webshot.deam.io/' + website + '?width=1280&height=720'
+    }
   }
 
   return (
-    <div className="Wrapper">
-      <Metatags />
-      <GithubCorner href="https://github.com/deamme/webshot/" />
-      <h1 className="brand">Webshot</h1>
-      <h2 className="description">Screenshot websites as a Service</h2>
-      <SearchBar
-        value={search}
-        onChange={e => setSearch(e.currentTarget.value)}
-        onKeyDown={e => {
-          if (e.keyCode === 13) {
-            setWebsite(search)
-            setSearch('')
-          }
-        }}
-      />
-      {link && (
-        <a className="link" href={link}>
-          {link.length > 100 ? link.substring(0, 64) + '...' : link}
-        </a>
-      )}
+    <div className="container">
+      <div className="wrapper">
+        <Metatags />
+        <GithubCorner href="https://github.com/deamme/webshot/" />
+        <h1 className="brand">Webshot</h1>
+        <h2 className="description">Screenshot websites as a Service</h2>
+        <SearchBar
+          value={search}
+          onChange={e => setSearch(e.currentTarget.value)}
+          onKeyDown={e => {
+            if (e.keyCode === 13) {
+              setWebsite(search)
+              setSearch('')
+            }
+          }}
+        />
+        <div className="options">
+          <button className={option === 0 ? 'active' : ''} onClick={() => setOption(0)}>
+            Default
+          </button>
+          <button className={option === 1 ? 'active' : ''} onClick={() => setOption(1)}>
+            Full page
+          </button>
+          <button className={option === 2 ? 'active' : ''} onClick={() => setOption(2)}>
+            1280x720
+          </button>
+        </div>
+        {link && (
+          <a className="link" href={link}>
+            {link.length > 100 ? link.substring(0, 64) + '...' : link}
+          </a>
+        )}
+      </div>
       <Gallery>
         <PreviewCard src={link} />
       </Gallery>
